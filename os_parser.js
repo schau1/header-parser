76c5740c4177c849b914045a99d2e75502ea4c84
schau1 <styrenev@gmail.com>
2016-07-25 20:49:14 -0700

    debugging heroku app crash deployment

--- os_parser.js
+++ os_parser.js
@@ -7,15 +7,15 @@ var app = express();
 
 app.get('/api/whoami', function(req,res){
     var json = {};
-    json.ipaddress = req.headers['x-forwarded-for'];
+/*    json.ipaddress = req.headers['x-forwarded-for'];
     json.language = req.headers['accept-language'].split(',')[0];
     
     var software = req.headers['user-agent'].split('(')[1].split(')');
 
-    json.software = software[0];
+    json.software = software[0];*/
 
     res.end(JSON.stringify(json));
 });
 
 console.log("Request Header Parser Microservice starting...")
-app.listen(process.env.PORT || 8080) 
+app.listen(process.env.PORT || 8080);
